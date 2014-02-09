18531f2379939c96e1a306db629d55c5fa7c157d
Ardo Illaste <ardo@sysbio.ioc.ee>
2014-02-09 13:42:29 +0100

    Find regions with larger window and step size Fix for max finding

--- lsjuicer/data/analysis/regionfinder.py
+++ lsjuicer/data/analysis/regionfinder.py
@@ -102,56 +102,60 @@ def _filter_ridge_lines2(cwt, ridge_lines, window_size=None, min_length=None,
 
     return list(filter(filt_func, ridge_lines))
 
-def find_peaks_cwt(vector, widths, min_snr=1):
-    #print vector.size, widths
-    gap_thresh = n.ceil(widths[0])
-    max_distances = widths / 3.0
-    wavelet = ss.ricker
-    pad = 1
-    if pad:
-        #vec = n.hstack((vector[:vector.size/pad+1][::-1], vector, vector[-vector.size/pad:][::-1]))
-        vec = pad_data_const(vector, pad)
-    else:
-        vec=vector
-    cwt_dat_all = ss.cwt(vec, wavelet, widths)
-    cwt_dat = cwt_dat_all[:,vector.size/pad:vector.size/pad+vector.size]
-    ridge_lines = ss._peak_finding._identify_ridge_lines(cwt_dat, max_distances, gap_thresh)
-    filtered = _filter_ridge_lines(cwt_dat, ridge_lines, min_snr=min_snr, min_length=5)
-    #if pad:
-    #    good_ones = [x for x in filtered if x[1][0]>vector.size/pad\
-    #                and x[1][0]<vector.size+vector.size/pad]
-    #else:
-    good_ones = filtered
-    #print '\n\ngood ones'
-    #for g in good_ones:
-    #    print g
-    #adjust = vector.size/pad - 1 if pad else 0
-    #find boundaries of region from its half height cwd by looking for local minima around the peak
-    max_locs = []
-    for g in good_ones:
-        loc = g[0][-1]*5/12
-        minima = n.argwhere(ss._peak_finding._boolrelextrema(cwt_dat[loc], n.less)).flatten()
-        x_loc = g[1][2]
-        left_min = 0
-        right_min = len(cwt_dat[loc])
-        for mi in minima:
-            if mi < x_loc and mi > left_min:
-                left_min = mi
-            if mi>x_loc and mi<right_min:
-                right_min = mi
-        max_locs.append((x_loc ,g[0][-1], left_min, right_min,loc))
-    #print 'mm',max_locs
-    return n.array(sorted(max_locs, key=lambda x:x[0]))
+#def find_peaks_cwt(vector, widths, min_snr=1):
+#    #print vector.size, widths
+#    gap_thresh = n.ceil(widths[0])
+#    max_distances = widths / 3.0
+#    wavelet = ss.ricker
+#    pad = 1
+#    if pad:
+#        #vec = n.hstack((vector[:vector.size/pad+1][::-1], vector, vector[-vector.size/pad:][::-1]))
+#        vec = pad_data_const(vector, pad)
+#    else:
+#        vec=vector
+#    cwt_dat_all = ss.cwt(vec, wavelet, widths)
+#    cwt_dat = cwt_dat_all[:,vector.size/pad:vector.size/pad+vector.size]
+#    ridge_lines = ss._peak_finding._identify_ridge_lines(cwt_dat, max_distances, gap_thresh)
+#    filtered = _filter_ridge_lines(cwt_dat, ridge_lines, min_snr=min_snr, min_length=5)
+#    #if pad:
+#    #    good_ones = [x for x in filtered if x[1][0]>vector.size/pad\
+#    #                and x[1][0]<vector.size+vector.size/pad]
+#    #else:
+#    good_ones = filtered
+#    #print '\n\ngood ones'
+#    #for g in good_ones:
+#    #    print g
+#    #adjust = vector.size/pad - 1 if pad else 0
+#    #find boundaries of region from its half height cwd by looking for local minima around the peak
+#    max_locs = []
+#    for g in good_ones:
+#        loc = g[0][-1]*5/12
+#        minima = n.argwhere(ss._peak_finding._boolrelextrema(cwt_dat[loc], n.less)).flatten()
+#        x_loc = g[1][2]
+#        left_min = 0
+#        right_min = len(cwt_dat[loc])
+#        for mi in minima:
+#            if mi < x_loc and mi > left_min:
+#                left_min = mi
+#            if mi>x_loc and mi<right_min:
+#                right_min = mi
+#        max_locs.append((x_loc ,g[0][-1], left_min, right_min,loc))
+#    #print 'mm',max_locs
+#    return n.array(sorted(max_locs, key=lambda x:x[0]))
 
 def find_first_max(vec_in):
     """find the first local maximum in a vector"""
-    i = 1
+    i = 0
     threshold = 1.05#peak cannot be more than treshold x neighbour value
     vec = n.diff(vec_in)
     remembered = None
     while i<len(vec)-2:
         if vec[i]>0 and vec[i]*vec[i+1]<0:
-            if min(vec[i]/vec[i-1], vec[i]/vec[i+1])<threshold:
+            check = []
+            check.append(vec[i]/vec[i+1])
+            if i>0:
+                check.append(vec[i]/vec[i-1])
+            if min(check)<threshold:
                 return i + 1
             else:
                 #we want the peak not to be more than 5% of neighbours
@@ -325,14 +329,14 @@ def find_regions(peaks, cwd_data):
             regions[p] = (minleft, minright)
     return regions
 
-def get_peaks(data, min_snr=3.5, max_width=75):
+def get_peaks(data, min_snr=3.5, max_width=150,step=5):
     #w = [1, 5, 10, 15, 20, 25, 50, 100, 150]
     #widths=n.array(w)
     #changed 2->4 for puffs
     #wlist = [1] + range(2,20,4)+range(20,120,5)
     #wlist = range(1,120,5)
     #widths = n.array(wlist)
-    widths =  n.arange(1,max_width ,2)
+    widths =  n.arange(1,max_width ,step)
     peaks_all = find_peaks_cwt2(data, widths, min_snr)
     if not peaks_all:
         return {}
@@ -349,10 +353,10 @@ def get_peaks(data, min_snr=3.5, max_width=75):
     #print 'found regions', regions
     return regions
 
-def show_peaks(data, min_snr=5.0, max_width = 50, xmin=None, xmax=None):
+def show_peaks(data, min_snr=5.0, max_width = 50, step = 2,xmin=None, xmax=None):
     import pylab
     pylab.plot(data)
-    regions = get_peaks(data, min_snr, max_width)
+    regions = get_peaks(data, min_snr, max_width,step)
     ca=pylab.gca()
     for r,b in regions.iteritems():
         pylab.plot(r,data.max(),'ro')
