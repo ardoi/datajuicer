a0d63909ab4d56f0f3523e12aa92d6376f78ea57
Ardo Illaste <ardo@sysbio.ioc.ee>
2014-07-22 17:25:22 +0200

    Start IPython cluster if one is not already running
    --HG--
    branch : nogui

--- lsjuicer/util/threader.py
+++ lsjuicer/util/threader.py
@@ -1,11 +1,12 @@
 from multiprocessing import cpu_count
 import time
 import random
+import traceback
 
 import numpy as np
+import pexpect
 
 import lsjuicer.util.logger as logger
-from lsjuicer.inout.converters.OMEXMLMaker import ShellRunner
 
 from IPython import parallel
 
@@ -23,20 +24,18 @@ def single(args):
 
 
 class Threader(object):
-    #threads_done = QC.pyqtSignal()
-    #progress_update = QC.pyqtSignal(int, int, int, int)
-    #time_stats = QC.pyqtSignal(float, float, float)
-    #new_progress_array = QC.pyqtSignal()
+
     def update(self):
         if self.start_time is None:
             self.start_time = time.time()
             #self.run_times []
 
-        self.logger.info("UPDATE")
+        self.logger.info("Threader update")
         if not self.view_result:
             self.logger.info("Start {} jobs".format(self.jobs_to_run))
             random.shuffle(self.params)
-            self.view_result = self.view.map_async(single, self.params, ordered=False)#, chunksize=self.chunk)
+            self.view_result = self.view.map_async(single, self.params, ordered=False,
+                                                   chunksize=self.chunk)
             self.waiting = set(self.view_result.msg_ids)
             self.failed = 0
         try:
@@ -71,7 +70,7 @@ class Threader(object):
                     self.logger.warning(traceback.format_exc())
 
         time_so_far = int(time.time() - self.start_time)
-        jobs_left = len(self.waiting)
+        jobs_left = len(self.waiting)*self.chunk
         jobs_done = self.jobs_to_run - jobs_left
         if jobs_done:
             time_per_job = time_so_far / float(jobs_done)
@@ -88,12 +87,6 @@ class Threader(object):
 
 
     def __init__(self):
-        #QC.QObject.__init__(self, parent)
-        self.waiting_workers = []
-        self.running_workers = []
-        self.finished_workers = []
-        self.failed_workers = []
-        self.workers = {}
         self.start_time = None
         self.end_time = None
         self.finished = False
@@ -103,19 +96,30 @@ class Threader(object):
         self.logger = logger.get_logger(__name__)
         try:
             self.client = parallel.Client()
-        except IOError:
+        except (IOError, parallel.error.TimeoutError):
             self.logger.warn("No cluster running. Trying to start")
-            self.runner = ShellRunner("ipcluster start -n {}".format(self.slots))
-            self.runner.start()
+            timeout = 10 #how long to wait for cluster to become available. IPython default is 30
+            self.runner = pexpect.spawn("ipcluster start -n {} --IPClusterEngines.early_shutdown={}".format(self.slots, timeout))
+            try:
+                cluster_start_time = time.time()
+                q = self.runner.expect(["Engines appear to have started successfully", "Cluster is already running"], timeout = timeout*2)
+                if q==0:
+                    self.logger.info("Cluster started with -n {} in {} seconds".format(self.slots, time.time() - cluster_start_time))
+                    self.client = parallel.Client()
+                else:
+                    raise RuntimeError()
+            except (pexpect.TIMEOUT, RuntimeError) as e:
+                self.logger.error("Cannot start cluster")
+                self.logger.error(traceback.format_exc(e))
+                raise RuntimeError("No cluster")
+
         self.view = self.client.load_balanced_view()
         self.logger.info("start Threader")
-        #self.view = self.client.direct_view()
-        #self.view.execute("import lsjuicer.data.analysis.transient_find as tf")
         self.view_result = None
         self.actual_slots = len(self.client.ids)
         self.results = {}
         self.errors = {}
-        self.chunk = 1
+        self.chunk = 10
         self.new_finished = False
 
 
@@ -123,9 +127,10 @@ class Threader(object):
         self.jobs_to_run = len(params)
         self.settings = settings
         self.params = params
+        selection = settings['selection']
         self.state_array = np.zeros(
-            shape=(settings['height'] - 2 * settings['dy'],
-                   settings['width'] - 2 * settings['dx']))
+            shape=(selection.height - 2 * settings['dy'],
+                   selection.width - 2 * settings['dx']))
 
     def run(self):
         self.update()
@@ -137,11 +142,16 @@ class Threader(object):
     def done(self):
         if not self.finished:
             self.finished = True
-            self.end_time = time.time()
-            av_time = (self.end_time - self.start_time)/float(self.jobs_to_run)
-            self.logger.info("time per job={}".format(av_time))
-            self.logger.info("time per job (actual)={}".format(av_time*self.actual_slots))
             self.client.close()
+            if self.start_time:
+                self.end_time = time.time()
+                av_time = (self.end_time - self.start_time)/float(self.jobs_to_run)
+                self.logger.info("time per job={}".format(av_time))
+                self.logger.info("time per job (actual)={}".format(av_time*self.slots))
+            if self.runner:
+                #self.client.shutdown(hub=True)
+                self.runner.terminate()
+                self.runner = None
 
 
 
